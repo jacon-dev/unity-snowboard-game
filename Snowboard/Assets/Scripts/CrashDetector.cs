8fd2de411132a50374b39ac64ffafd8922a9864e
James Conlin <jacon.dev@outlook.com>
2023-09-02 10:29:54 +0100

    Adding scene loading when crashing or finishing the level using invoking

--- Snowboard/Assets/Scripts/CrashDetector.cs
+++ Snowboard/Assets/Scripts/CrashDetector.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private readonly float sceneLoadDelay = 0.5f;
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Ground")
         {
-            Debug.Log("You hit your head! Game over!!");
+            Invoke(nameof(ReloadScene), sceneLoadDelay);
+            
         }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Snowboard/Assets/Scripts/FinishLine.cs
+++ Snowboard/Assets/Scripts/FinishLine.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    private readonly float sceneLoadDelay = 0.5f;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            Debug.Log("Level Completed!!");
+            Invoke(nameof(LoadNextScene), sceneLoadDelay);
         }
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
